3af314149237280a14c48f81fdbff41ac8db4fc5
Loic <loicwinkopp@gmail.com>
2018-12-09 19:37:19 -0500

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: '675422435818-rjuurcivqjmta672bs4du3m67sarad6g.apps.googleusercontent.com',
   googleClientSecret: 'AFeUp_F2drCYxCfBCuTQhIDg',
-  mongoURI: 'mongodb://loic:learning1@ds011314.mlab.com:11314/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
